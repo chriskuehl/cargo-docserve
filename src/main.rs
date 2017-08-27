46b74b16b1e291de3626b8ef841548aea3d9f84d
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2017-08-27 15:47:25 -0700

    Make argument parsing expect to be a cargo subcommand

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ extern crate cargo_docserve;
 
 use cargo_docserve::Config;
 use clap::App;
+use clap::AppSettings;
 use clap::Arg;
+use clap::SubCommand;
 use std::net::AddrParseError;
 use std::net::SocketAddr;
 
@@ -20,17 +22,22 @@ fn main() {
     let matches = App::new("cargo-docserve")
         .version(env!("CARGO_PKG_VERSION"))
         .about("build and serve cargo documentation with a static file server")
-        .arg(Arg::with_name("bind")
-             .help("The bind address and port, e.g. 0.0.0.0:8888")
-             .long("bind")
-             .short("b")
-             .default_value("0.0.0.0:8888")
-             // TODO: is there some way to get clap to turn this into the right type for me?
-             // It seems silly to parse it once to validate and then again to get the actual value.
-             .validator(validate_bind)
-             .takes_value(true)
+        .settings(&[AppSettings::SubcommandRequired])
+        .bin_name("cargo")
+        .subcommand(SubCommand::with_name("docserve")
+            .arg(Arg::with_name("bind")
+                 .help("The bind address and port, e.g. 0.0.0.0:8888")
+                 .long("bind")
+                 .short("b")
+                 .default_value("0.0.0.0:8888")
+                 // TODO: is there some way to get clap to turn this into the right type for me?
+                 // It seems silly to parse it once to validate and then again to get the actual value.
+                 .validator(validate_bind)
+                 .takes_value(true)
+            )
         )
         .get_matches();
+    let matches = matches.subcommand_matches("docserve").unwrap();
 
     let addr = matches.value_of("bind").unwrap().parse().unwrap();
     let conf = Config { addr };
